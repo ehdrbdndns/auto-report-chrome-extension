0a9dfb31437a3e2cd99501ecf74c3c510de698d9
ehdrbdndns <60888473+ehdrbdndns@users.noreply.github.com>
2024-05-25 16:13:18 +0900

    [tracking url] chrome tab의 URL을 추척합니다.

--- public/popup.ts
+++ public/popup.ts
@@ -1,3 +1,36 @@
-chrome.storage.local.get({ urls: [] }, (result) => {
-  console.log(JSON.stringify(result));
+type linkInfo = { url: string; favIconUrl: string; title: string };
+chrome.storage.local.get(['urls'], (result) => {
+  const linkInfoList = Object.values(result.urls || {}) as linkInfo[];
+  console.log(result);
+  console.log(linkInfoList);
+
+  const list = document.getElementById('list');
+
+  for (let linkInfo of linkInfoList) {
+    const item = document.createElement('li');
+    item.classList.add('list-item');
+
+    const url = document.createElement('div');
+    url.classList.add('url');
+
+    const favicon = document.createElement('img');
+    favicon.src = linkInfo.favIconUrl;
+    favicon.classList.add('favicon');
+
+    const domain = document.createElement('span');
+    domain.classList.add('domain');
+    domain.innerText = linkInfo.title || linkInfo.url;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-btn');
+    deleteButton.innerText = 'X';
+
+    url.appendChild(favicon);
+    url.appendChild(domain);
+
+    item.appendChild(url);
+    item.appendChild(deleteButton);
+
+    list?.appendChild(item);
+  }
 });
--- src/background.ts
+++ src/background.ts
@@ -1,33 +1,50 @@
 chrome.tabs.onCreated.addListener((tab) => {
-  console.log('Tab created:', tab);
+  const { url, status } = tab;
 
   // Check if the tab has a URL
-  if (!tab.url) {
+  if (!url || status !== 'complete') {
     return;
   }
 
-  storeUrlToStorage(tab.url);
+  storeUrlToStorage(tab);
 });
 
 chrome.tabs.onUpdated.addListener((_, changeInfo, tab) => {
-  console.log('Tab updated:', tab);
+  const { url, status } = tab;
 
   // Check if the tab has a URL
-  if (!changeInfo.url || !tab.url) {
+  if (!url || status !== 'complete') {
     return;
   }
 
-  storeUrlToStorage(tab.url);
+  storeUrlToStorage(tab);
 });
 
-const storeUrlToStorage = (url: string) => {
-  console.log('Storing URL:', url);
+const storeUrlToStorage = (tab: chrome.tabs.Tab) => {
+  console.log('Tab updated:', tab);
+  const { id, url, favIconUrl, title } = tab;
+
+  if (!id) {
+    return;
+  }
 
   // Retrieve the list of URLs from storage
-  chrome.storage.local.get({ urls: [] }, (result) => {
-    const updatedUrls = [...result.urls, { url, timestamp: Date.now() }];
+  chrome.storage.local.get(['urls'], (result) => {
+    if (!result.urls) {
+      result.urls = {};
+    }
+
+    if (!result.urls[id]) {
+      result.urls[id] = {};
+    }
+
+    result.urls[id] = { url, favIconUrl, title };
+
+    console.log(result.urls);
 
     // Save the updated list of URLs to storage
-    chrome.storage.local.set({ urls: updatedUrls });
+    chrome.storage.local.set({
+      urls: { ...result.urls },
+    });
   });
 };
