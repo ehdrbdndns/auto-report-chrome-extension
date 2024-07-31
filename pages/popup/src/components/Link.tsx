a7f2d2989a74b3d32583168f5c6d31786306563b
ehdrbdndns <ehdrbdndns@naver.com>
2024-07-31 23:02:43 +0900

    [layout] Link Component 생성

---
+++ pages/popup/src/components/Link.tsx
@@ -0,0 +1,12 @@
+export default function Link() {
+  return (
+    <div>
+      {/* Favicon */}
+      <img src="" alt="" />
+      {/* URL */}
+      <span>https://naver.com</span>
+      {/* Delete Button */}
+      <button>x</button>
+    </div>
+  );
+}
