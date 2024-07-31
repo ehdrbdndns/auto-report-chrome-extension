fb73c68cd9d46ffa0550b49905abb65a2f6825b7
ehdrbdndns <ehdrbdndns@naver.com>
2024-07-31 23:02:26 +0900

    [layout] Content Wrapper 레이아웃 구성

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -1,16 +1,38 @@
 import '@src/Popup.css';
 import { withErrorBoundary, withSuspense } from '@extension/shared';
+import ContentWrapper from './components/ContentWrapper';
+import Link from './components/Link';
+
+const RIGHT_IMG_PATH = 'popup/right-arrow.svg';
+const INFO_IMG_PATH = 'popup/info.svg';
+
+const Info = () => {
+  return (
+    <div className="hover:cursor-pointer">
+      <img src={chrome.runtime.getURL(INFO_IMG_PATH)} alt="info img" />
+    </div>
+  );
+};
 
 const Popup = () => {
   return (
     <section>
       {/* Header */}
-      <header></header>
+      <header className="block mb-[3.2rem]">
+        <h1 className="text-xl font-bold black">자동 보고서 생성기</h1>
+      </header>
       {/* Main */}
-      <div>
+      <div className="flex gap-[1.6rem]">
         {/* Left */}
-
+        <ContentWrapper title="추적중인 링크" titleActionEl={<Info />}>
+          <div>
+            <Link></Link>
+          </div>
+        </ContentWrapper>
+        {/* Image */}
+        <img src={chrome.runtime.getURL(RIGHT_IMG_PATH)} alt="right arrow img" />
         {/* Right */}
+        <ContentWrapper title="분류한 링크"></ContentWrapper>
       </div>
     </section>
   );
