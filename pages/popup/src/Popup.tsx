51aefc9645516e9f7620839ff6d83e953a1f2f4c
ehdrbdndns <ehdrbdndns@naver.com>
2024-08-01 23:56:56 +0900

    [layout] small button 구성

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -2,6 +2,7 @@ import '@src/Popup.css';
 import { withErrorBoundary, withSuspense } from '@extension/shared';
 import ContentWrapper from './components/ContentWrapper';
 import Link from './components/Link';
+import { SmallButton } from './components/Button';
 
 const RIGHT_IMG_PATH = 'popup/right-arrow.svg';
 const INFO_IMG_PATH = 'popup/info.svg';
@@ -18,7 +19,7 @@ const Popup = () => {
   return (
     <section>
       {/* Header */}
-      <header className="block mb-[3.2rem]">
+      <header className="block mb-[1.6rem]">
         <h1 className="text-xl font-bold black">자동 보고서 생성기</h1>
       </header>
       {/* Main */}
@@ -33,7 +34,7 @@ const Popup = () => {
         {/* Image */}
         <img src={chrome.runtime.getURL(RIGHT_IMG_PATH)} alt="right arrow img" />
         {/* Right */}
-        <ContentWrapper title="분류한 링크"></ContentWrapper>
+        <ContentWrapper title="분류한 링크" titleActionEl={<SmallButton text="자동분류" />}></ContentWrapper>
       </div>
     </section>
   );
