d90e73fdc0dd9fbac6f40dded4857654e0a5d986
ehdrbdndns <ehdrbdndns@naver.com>
2024-09-02 14:56:45 +0900

    [storage] config link storage

---
+++ packages/storage/lib/linkStorage.ts
@@ -0,0 +1,40 @@
+import { BaseStorage, createStorage } from './base';
+
+export type LinkType = {
+  [url: string]: {
+    title: string; // URL Title
+    lastAccessed: number; // 가장 최근 방문한 시간, Unix Timestamp
+    favIconUrl: string; // favicon URL
+    visitedCount: number; // 방문 횟수
+    duration: number; // 방문 시간
+  };
+};
+
+type LinkStorage = BaseStorage<LinkType> & {
+  updateLink: (url: string, linkData: LinkType[string]) => Promise<void>;
+  deleteLink: (url: string) => Promise<void>;
+  retrieveLink: (url: string) => Promise<LinkType[string]>;
+};
+
+const storage = createStorage<LinkType>('url-storage-key', {});
+
+export const linkStorage: LinkStorage = {
+  ...storage,
+  updateLink: async (url: string, linkData: LinkType[string]) => {
+    await storage.set(cache => {
+      cache[url] = linkData;
+      return cache;
+    });
+  },
+  deleteLink: async (url: string) => {
+    await storage.set(cache => {
+      delete cache[url];
+      return cache;
+    });
+  },
+  retrieveLink: async (url: string) => {
+    return storage.get().then(cache => {
+      return cache[url];
+    });
+  },
+};
